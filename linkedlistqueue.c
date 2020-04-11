96ab1c567cbf2dfb7eac4c9e3096637d96fb309d
Erkan Giray Arat <61563120+erkangiray@users.noreply.github.com>
2020-04-11 23:29:46 +0300

    Add files via upload

--- linkedlistqueue.c
+++ linkedlistqueue.c
@@ -1,131 +1,90 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
 typedef struct {
 	int* p_data;
 	int p_priority;
 }item_t;
 
-typedef struct {
-	int size, index;
-	item_t** item;
-}queue_t;
-
-/*struct Node{
+typedef struct Node{
 	item_t* item;
-	struct Node* next;
-};
+	Node* next;//demek ki neymiþ typedef struct yapacaksan onun içinde ayný sturct'ý kullanmayacaksýn þayet öyle yaparsan compiler hata verir ve geri döner her yere struct Node* yazarsýn Teþekkürler Türkiye
+}Node;
 
-void addIntNode(struct Node** head, int data, int priority,int *count) {
-	if ((*count) > 100) {
-		printf("\nQueue has reached the maximum element limit of 100 elements.\n");
+// Function to Create A New Node 
+void push(Node** r_head , item_t *r_item)
+{
+	Node* newnode = (Node*)(malloc(sizeof(Node)));//allocate space for newnode
+	newnode->next = NULL;//next null
+	*(newnode->item->p_data) = *(r_item->p_data);//newnode's item's p_data's pointed value equals r_item's p_data's pointed value
+	newnode->item->p_priority = r_item->p_priority;
+	
+	if (r_head == NULL) {//if list is empty make head pointer the new node
+		*r_head = newnode;
 		return;
 	}
-	struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
-	item_t* newitem = (item_t*)malloc(sizeof(item_t));
-	int* new_data = malloc(sizeof(int));
-
-	*new_data = data;
-
-	(*newitem).p_priority = priority;
-	(*newitem).p_data = new_data;//!!!Somehow p_data does not get typecast into int, so I use int*workaround
-	(*newitem).workaround = (*newitem).p_data;
-
-	(*newnode).next = NULL;
-	(*newnode).item = newitem;
-
-	if ((*head) == NULL) {
-		*head = newnode;
-	}
-
-	else {
-		newnode->next = *head;//head points to current first entry, newnode's next equals head
-		*head = newnode;//head points to newnode
-	}
-
+	Node* temp; temp = *r_head;
+	while (((newnode->item->p_priority) < (temp->next->item->p_priority))||temp->next!=NULL) 
+		temp = temp->next;
+	//with this loop we make sure that temp now points to a node
+	//whose next pointer points to a node which has a priority equal to or smaller than the newnode
+	//
+	newnode->next = temp->next;//newnode points to smaller or equal priority node
+	temp->next = newnode;//higher priority node is made to point to newnode by accessing through temp, the next ptr
 }
 
+void pop(Node** r_head) {
+	
+	if (r_head = NULL) {//nothing to pop
+		printf("List is empty.\n");
+		return;
+		Node* temp;
+		temp = *r_head; *r_head = (*r_head)->next; free(temp);//highest priority is always at the beginning of the queue
+	}
+}
 
-void printList(struct Node** head, int const* count) {
-	if ((*head) == NULL) {
+void print(Node**r_head){
+	Node* temp;
+	temp = *r_head;
+	if (temp == NULL) {
 		printf("List is empty.\n");
 		return;
 	}
-	struct Node* temp;
-	temp = *head;
-	for (int i = 1; i <= *count; i++) {
-		printf("%d\n", *(temp->item->workaround));
+	while (temp->next != NULL) {
+		printf("Priority:%d Data:%d", temp->item->p_priority, *(temp->item->p_data));
 		temp = temp->next;
 	}
-}*/
-
-void init_qu(queue_t* queue1,int size) {
-	queue1->size = size;
-	queue1->index = 0;
-	(*queue1).item = (item_t**)malloc(size*sizeof(item_t*));//dynamically allocate memory for size times item_t pointers
-
-}
-
-int queue_add(queue_t* p_queue, item_t* p_item) {
-	if (p_queue->index == p_queue->size) {
-		printf("Queue is full.\n");
-		return 1;
-	}
-
-	*((*(p_queue->item) + (p_queue->index) * sizeof(item_t))->p_data) = *(p_item->p_data);
-	//p_queue->item[p_queue->index] = p_item;
-	p_queue->index++;
-	return 0;
-}
-
-void printQ(queue_t* p_queue) {
-	if (p_queue->index == 0) {
-		printf("Queue is empty.\n");
-		return;
-	}
-	int temp_index = p_queue->index;
-	while (temp_index >= 0) {
-		printf("Priority:%d  Data:%d\n", (*(p_queue->item)+ (p_queue->index) * sizeof(item_t))->p_priority, *((*(p_queue->item) + (p_queue->index) * sizeof(item_t))->p_data));
-		temp_index--;
-	}
 }
 
 
-int main() {
-	queue_t queue1;
-	init_qu(&queue1, 100);
-
-	item_t item1;
-	item1.p_data = (int*)malloc(sizeof(int));
-	*item1.p_data = 15;
-	item1.p_priority = 0;
-
-	if (queue_add(&queue1, &item1) == 0)
-		printf("Item has correctly been queued.\n");
-	
-	item_t item2;
-	item2.p_data = (int*)malloc(sizeof(int));
-	*item2.p_data = 15;
-	item2.p_priority = 0;
-	if (queue_add(&queue1, &item2) == 0)
-		printf("Item has correctly been queued.\n");
-
-	item_t item3;
-	item3.p_data = (int*)malloc(sizeof(int));
-	*item3.p_data = 15;
-	item3.p_priority = 0;
-	if (queue_add(&queue1, &item3) == 0)
-		printf("Item has correctly been queued.\n");
-
-	item_t item4;
-	item4.p_data = (int*)malloc(sizeof(int));
-	*item4.p_data = 15;
-	item4.p_priority = 0;
-	if (queue_add(&queue1, &item4) == 0)
-		printf("Item has correctly been queued.\n");
-
-	printQ(&queue1);
+int main()
+{
+	Node* head = NULL;
+	item_t item1; *(item1.p_data) = 15; item1.p_priority = 1;
+	item_t item2; *(item2.p_data) = -15; item2.p_priority = 0;
+	item_t item3; *(item3.p_data) = 12; item3.p_priority = 1;
+	item_t item4; *(item4.p_data) = -28; item4.p_priority = 0;
+	item_t item5; *(item5.p_data) = 5; item5.p_priority = 1;
+	item_t item6; *(item6.p_data) = 155; item6.p_priority = 2;
+	item_t item7; *(item7.p_data) = 286; item7.p_priority = 2;
+	item_t item8; *(item8.p_data) = 375; item8.p_priority = 2;
+	item_t item9; *(item9.p_data) = 3; item9.p_priority = 1;
+	item_t item10; *(item10.p_data) = -45; item10.p_priority = 0;
+	item_t item11; *(item11.p_data) = -83; item11.p_priority = 0;
+	item_t item12; *(item12.p_data) = -157; item12.p_priority = 0;
+	push(&head, &item1);
+	push(&head, &item2);
+	push(&head, &item3);
+	push(&head, &item4);
+	push(&head, &item5);
+	push(&head, &item6);
+	push(&head, &item7);
+	push(&head, &item8);
+	push(&head, &item9);
+	push(&head, &item10);
+	push(&head, &item11);
+	push(&head, &item12);
+	printf(&head);
 
 
 
